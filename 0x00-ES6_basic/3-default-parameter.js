495574ef990e6e928c7f63ef98f3d3a51c9cd927
Zaya <heavenphrince@gmail.com>
2024-01-02 21:35:00 +0100

    2 5th test adjusted  eslint disable and enable added to each line in if

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,3 @@
-/* use of default parameters */
-
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
