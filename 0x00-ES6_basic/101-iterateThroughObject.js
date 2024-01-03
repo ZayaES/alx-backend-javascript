8271dfe9a375b3ecae2308894ad86966bf5570ea
Zaya <heavenphrince@gmail.com>
2024-01-03 02:12:04 +0100

    14 1st test

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = "";
-  for (let x of reportWithIterator) {
+  let employeeNames = ``;
+  for (const x of reportWithIterator) {
     employeeNames += `${x} | `;
   }
   return employeeNames.slice(0, -3);
