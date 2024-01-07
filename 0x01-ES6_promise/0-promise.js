c470f7965d2a66a0d0b86e0397d4148adf735fd9
Zaya <heavenphrince@gmail.com>
2024-01-07 19:51:30 +0100

    2 1st test

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise (function(){});} 
-	/*(resolve, reject) {
-    if (1) {
-	resolve("yay a promise");
-    } else {
-      reject("nay");
-    }	
-  });
-}*/
+  return new Promise(() => {});
+} 
