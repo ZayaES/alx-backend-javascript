ee8752966c2fa87190049a4c3442e50613948842
Zaya <heavenphrince@gmail.com>
2024-01-09 12:51:12 +0100

    2-2nd

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(array, city) {
   function checkLocation(arr) {
-    arr.location === city
+    return arr.location === city
   }
   return array.filter(checkLocation);
 }
