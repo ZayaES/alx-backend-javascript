eeca8d9d40b943bc40f22977473bd5f2789e4d56
Zaya <heavenphrince@gmail.com>
2024-01-10 23:43:34 +0100

    8 1st

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,9 @@
-export default function cleanSet (set, startString) {
+export default function cleanSet(set, startString) {
   if (!startString) {
     return '';
   }
   const array = [...set];
   let arr = array.filter((entry) => entry.startsWith(startString));
-  arr = arr.map((entry) => {
-    return entry.slice(startString.length);
-  });
+  arr = arr.map((entry) => entry.slice(startString.length));
   return arr.join('-');
 }
