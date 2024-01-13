1b6cf8c98ab92445cccebcfd4f33915ee7ea0ad4
Zaya <heavenphrince@gmail.com>
2024-01-13 03:17:38 +0100

    10 -1st

--- 0x03-ES6_data_manipulation/10-main.js
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -1,4 +1,4 @@
-iimport updateUniqueItems from "./10-update_uniq_items.js";
+import updateUniqueItems from "./10-update_uniq_items.js";
 import groceriesList from "./9-groceries_list.js";
 
 const map = groceriesList();
