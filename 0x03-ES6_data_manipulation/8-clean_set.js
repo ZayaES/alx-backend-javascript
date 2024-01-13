7f3b8b1b8ae5317db3dda124fef0e45cf49469ea
Zaya <heavenphrince@gmail.com>
2024-01-13 02:45:04 +0100

    yo yo yo

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
     return '';
   }
   const array = [...set];
-  let arr = array.filter((entry) => entry.startsWith(startString));
-  arr = arr.map((entry) => entry.slice(startString.length));
+  let arr = array.filter((entry) => (entry !== undefined ? entry.startsWith(startString) : ''));
+  arr = arr.map((entry) => (entry !== undefined ?entry.slice(startString.length) : ''));
   return arr.join('-');
 }
